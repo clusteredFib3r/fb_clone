90eeb4e5e050c235cf4950d0eef9db162169e1af
clusteredFib3r <clusteredFiber@gmail.com>
2018-10-30 11:54:41 -0400

    Started facebook clone..

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!-- facebook clone by clusteredFib3r -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Facebook - Log In or Sign Up</title>
+    <link rel="shortcut icon" href="./images/fb.ico">
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <div class="main-wrapper">
+      <!-- main header begin -->
+      <header class="main-header">
+        <div class="fit-middle">
+          <div class="logo-side">
+            <h1><a href="#">facebook</a></h1>
+          </div>
+          <div class="login-side">
+
+          </div>
+        </div>
+      </header>
+      <!-- main header end -->
+
+      <!-- main body begin -->
+      <main class="main-body">
+        <div class="fit-middle">
+          <div class="main-left">
+            <h2>Connect with friends and the <br> world around you on Facebook.</h2>
+
+            <div class="feature-item">
+              <div class="feature-image">
+                <img src="./images/1.png" alt="" style="vertical-align: middle">
+              </div>
+              <div class="feature-text">
+                <span class="important">See photos and updates</span>
+                <span class="regular">from friends in news feed.</span>
+              </div>
+            </div>
+
+            <div class="feature-item">
+              <div class="feature-image">
+                <img src="./images/2.png" alt="" style="vertical-align: middle">
+              </div>
+              <div class="feature-text">
+                <span class="important">Share what's new</span>
+                <span class="regular">in your life on your Timeline.</span>
+              </div>
+            </div>
+
+            <div class="feature-item">
+              <div class="feature-image">
+                <img src="./images/3.png" alt="" style="vertical-align: middle">
+              </div>
+              <div class="feature-text">
+                <span class="important">Find more</span>
+                <span class="regular">of what you're looking for with Facebook Search.</span>
+              </div>
+            </div>
+          </div>
+          <div class="main-right">
+
+          </div>
+        </div>
+      </main>
+      <!-- main body end -->
+
+      <!-- main footer begin -->
+      <!-- main footer end -->
+    </div>
+  </body>
+</html>
